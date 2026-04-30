661adaa6806489ee6b959f5cc9499a952888945f
Koffi Simplice ABOU <asimplice@ebenyx.com>
2026-04-30 17:06:23 +0000

    Fix CORS - désactiver allowCredentials

--- src/main/java/com/lyceeande/inscription/config/CorsConfig.java
+++ src/main/java/com/lyceeande/inscription/config/CorsConfig.java
@@ -10,9 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*") // Autorise toutes les adresses (IP, localhost, mobile)
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false); // ← changez true en false
     }
 }
